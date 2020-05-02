12d4da6eac59d316b026db469a128b4ec6bd6167
Christopher Fujino <christopherfujino@gmail.com>
2020-05-02 16:57:53 -0700

    render fetched text

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Hello, world!");
+/** Fetch endpoint and return response text */
+async function fetcher(endpoint, init = {}) {
+  try {
+    const response = await window.fetch(endpoint, init);
+    const text = await response.text();
+    if (!response.ok) {
+      console.error(`Fetch failed with code ${response.status}!`);
+      console.error(text);
+      return response;
+    }
+    return text;
+  } catch(err) {
+    console.error("Fetch failed!");
+    console.log(err);
+  }
+}
+
+function render(data) {
+  const div = document.createElement("div");
+  div.innerText = data;
+  return div;
+}
+
+async function main() {
+  const url =
+    "https://raw.githubusercontent.com/christopherfujino/dotfiles/master/.vimrc";
+  const raw = await fetcher(url);
+
+  console.log(raw);
+  const div = render(raw);
+  document.body.append(div);
+}
+
+main();
